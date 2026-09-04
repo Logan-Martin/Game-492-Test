04ce7228dfcde1ac6830d5d599284969f5f866d6
Logan Martin <loganmartinemail@gmail.com>
2026-09-04 14:05:15 -0400

    Started work on NewInputSystem
    Started w/ known C# Class-based implementation. Need to switch to Global implementation.
    
    Notes reflect the class-based system.

--- UnityProj/Game 492 Test/Assets/Jammo-Character/Scripts/CharacterSkinController.cs
+++ UnityProj/Game 492 Test/Assets/Jammo-Character/Scripts/CharacterSkinController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class CharacterSkinController : MonoBehaviour
 {
@@ -13,6 +14,35 @@ public class CharacterSkinController : MonoBehaviour
     public enum EyePosition { normal, happy, angry, dead}
     public EyePosition eyeState;
 
+    // -------- //
+    // Variables for NewInputSystem
+    // Note: This Script on changes the face of the player character, not movement
+    public InputSystem_Actions playerControls; 
+            // auto under Assets folder by Unity when using new input system. Named InputSystem_Actions
+
+    // public for ease of click-and-drag refrence
+
+    private InputAction EyePositionAction_Normal;
+    private InputAction EyePositionAction_Angry;
+    private InputAction EyePositionAction_Happy;
+    private InputAction EyePositionAction_Dead;
+        // Note: ever InputAction needs to be:
+            // 1. set up w/ the type itself as a variable 
+            // 2. assigned/connected to an event under the OnEnabled() event w/ the following code:
+                    // variable = variable.Category.NameOfAction;
+                    // variable.performed += RelatedFunction;
+                            // !Note: Each RelatedFunction must start w/ an 'InputAction.CallbackContext' argument type
+
+        // Note: For individual InputActions to work, I think there always needs to be the overall InputSystem_Actions
+        // That setup requires the following: [In only one? script or anytime used in a script?]
+            // 1. variable [of InputActionAsset type] assigned on Awake() event:
+                    // Ex: playerControls = new InputActionAsset();
+            // 2. same needs to be connected to OnDisable() event:
+                    // Ex: playerControls.Disable();
+
+    // 000000000 //
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -20,6 +50,21 @@ public class CharacterSkinController : MonoBehaviour
         characterMaterials = GetComponentsInChildren<Renderer>();
         
     }
+
+    private void Awake()
+    {
+        playerControls = new InputActionAsset();
+    }
+
+    private void OnEnable()
+    {
+        playerControls.Enable();
+
+        EyePositionAction_Normal = playerControls
+        EyePositionAction_Angry
+        EyePositionAction_Happy
+        EyePositionAction_Dead
+}
 
     // Update is called once per frame
     void Update()
